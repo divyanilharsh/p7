a0c33b7126baa70474429e81539372be89c5f049
divyanilharsh <68350726+divyanilharsh@users.noreply.github.com>
2025-08-22 07:04:41 +0530

    improved maecket sumalater

---
+++ line_by_line_explanation.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4b0366d719cf518c22e39eb0db32d2489b549edf91d3b8f45db7bda8eb00ae0
+size 44720
--- marcketsumalation/sumalate.py
+++ marcketsumalation/sumalate.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2dd088a442eb2b1916f7c3e7ec4df60ee776428b6b0d97cebdb29c00a02898
-size 13421
+oid sha256:b6706817e97a764f92626ae7b0b2f8f134aa8fe0c349f3a18792e61d1eb5c595
+size 21141
